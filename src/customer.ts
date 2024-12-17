c96d6a744a92c19bdfd17decb97eef5e2f55be49
chicoumbetov <shyngysmaaan@gmail.com>
2024-12-17 08:59:17 +0100

    :bug: fix customer rpc

--- src/customer.ts
+++ src/customer.ts
@@ -1,21 +1,32 @@
 import express from "express";
 
-const { RPCObserver } = require("./rpc");
+const { RPCObserver, RPCRequest } = require("./rpc");
 const { app } = require("./socket/server");
 const PORT = 9000;
 
 app.use(express.json());
 
 const fakeCustomerResponse = {
-  _id: "klsdjqfmlkd,sqfmljd",
+  _id: "yt686tu8763TYYR98734",
   name: "Mike",
   country: "Poland",
 };
 
 RPCObserver("CUSTOMER_RPC_QUEUE", fakeCustomerResponse);
 
-app.get("/wishlist", (req: any, res: any) => {
-  return res.json("Customer Service");
+app.get("/wishlist", async (req: any, res: any) => {
+  const requestPayload = {
+    productId: "123",
+    customerId: "yt686tu8763TYYR98734",
+  };
+  try {
+    const respData = await RPCRequest("PRODUCTS_RPC_QUEUE", requestPayload);
+    console.log("Customer wishlist response data : ", respData);
+    return res.status(200).json(respData);
+  } catch (error) {
+    console.log("Customer wishlist error: ", error);
+    return res.status(500).json(error);
+  }
 });
 
 app.get("/", (req: any, res: any) => {
